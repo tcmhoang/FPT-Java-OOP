441b2dc5cd0ae3653e3114af01329f419a886312
Cameron Darren <51996720+Artlist-creator@users.noreply.github.com>
2020-01-13 19:38:53 -0500

    Delete unused getters, setters

--- CandidateManagement/src/Candidate.java
+++ CandidateManagement/src/Candidate.java
@@ -27,64 +27,28 @@ public class Candidate {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getFirstName() {
         return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     public int getBirthDate() {
         return birthDate;
-    }
-
-    public void setBirthDate(int birthDate) {
-        this.birthDate = birthDate;
     }
 
     public String getAddress() {
         return address;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
     public String getPhone() {
         return phone;
     }
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getTypeCandidate() {
-        return type;
-    }
-
-    public void setTypeCandidate(int typeCandidate) {
-        this.type = typeCandidate;
     }
 
     @Override
