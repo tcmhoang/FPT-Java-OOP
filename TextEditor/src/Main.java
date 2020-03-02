5d2329621cc53de1dc0e1659961230e45feb117c
Cameron Darren <51996720+Artlist-creator@users.noreply.github.com>
2020-03-02 11:21:35 -0500

    Finish java swing simple text-editor

---
+++ TextEditor/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        TextEditor textEditor = new TextEditor();
+        textEditor.setVisible(true);
+    }
+}
